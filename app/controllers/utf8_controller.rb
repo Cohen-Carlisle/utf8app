0c5e8ae06451f88718f4c517b072ba1459560f57
Cohen Carlisle <cohencarlisle+github@protonmail.com>
2018-12-19 21:39:02 -0500

    utf8 json example

--- app/controllers/utf8_controller.rb
+++ app/controllers/utf8_controller.rb
@@ -2,4 +2,8 @@ class Utf8Controller < ApplicationController
   def example
     render plain: 'The symbol for the foot unit of measurement is ′.'
   end
+
+  def json_example
+    render json: {feet: '′', inches: '″'}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get '/', to: 'utf8#example'
+  get '/json', to: 'utf8#json_example'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
